27efc5987158b2065d2a57e2b4b61419d2d1099c
Punam Deore <104068944+PunamDeore@users.noreply.github.com>
2024-04-24 14:56:33 +0530

    dynamic data binding

--- src/app/certificate/certificate.component.ts
+++ src/app/certificate/certificate.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-certificate',
   styleUrls: ['./certificate.component.scss'],
   templateUrl: './certificate.component.html',
-  standalone:true
+  standalone:true,
+  imports: [CommonModule]
 })
 export class CertificateComponent {
   Data = {
@@ -20,7 +22,15 @@ export class CertificateComponent {
     orgAddress: "Office No. 404, Vastu Viva, Beside Sanket Inn, Bhumkar Chowk, Wakad, Pune, Maharashtra - 411057",
   };
   config ={
-    certificateNumber:'string',
-    certificateLogo:'string',
+    certificateNumber:'srno',
+    companyLogo: 'logo',
+    certificateLogo:'logo1',
+    recipientName: 'candidateName',
+    courseName: 'course',
+    completionDate: 'date',
+    presidentSignature:'signature',
+    directorSignature: 'signature1',
+    orgName: 'org-name',
+    orgAddress: 'org-address',
   }
 }
